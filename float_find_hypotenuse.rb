f26fa5eb617290f77bd43a616c5f4d7cfe85770b
William Price <price.james.william@gmail.com>
2023-03-22 19:31:57 +0000

    float

--- float_find_hypotenuse.rb
+++ float_find_hypotenuse.rb
@@ -1,8 +1,12 @@
 # A right triangle's two shorter sides have length 2.8 and 4.5. How long is its longest side?
 # Check the pythagorous.png file for the Pythagorean Theorem
-# 
+#
 # Your output should be a number; for example,
-# 
+#
 # 5.3
 
+short_1 = 2.8
+short_2 = 4.5
 
+longest_side = Math.sqrt(short_1 ** 2 + short_2 ** 2)
+puts longest_side
--- float_round.rb
+++ float_round.rb
@@ -1,2 +1,5 @@
 # Output ten divided by 3 rounded to 3 decimal places.
-  
+
+answer = 10.0 / 3
+rounded_results = answer.round(3)
+puts rounded_results
